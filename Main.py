19421a27ee818727bd1a76f2afb45e187a06a682
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-03-19 13:41:22 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from time import sleep
+from pass import uname,pw
+
+
+class InstaBot:
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome()
+        self.username = username
+        self.driver.get("https://instagram.com")
+        sleep(2)
+        self.driver.find_element_by_class_name("f0n8F")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(2)
+        
+InstaBot(uname,pw)
